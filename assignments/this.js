3572a1b64ce64e8737150bf6d4af0d343509059f
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-08-21 12:45:28 +0100

    The 4 principles of this-keyword explained

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,17 @@
-/* The for principles of "this";
+/* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. this - window/undefined object binding
+        here this is accessing the window object and as such it is global and had access to all the methods of the Window object.It is usually not confined in the body of any object, it can be found in what we may refer to as TOPLEVELVARIABLES; Variables which have a global scope
+
+* 2. this - implicit binding
+        In this case, "this" refers to the object that it is in. 
+
+* 3. this - new keyword binding
+        Here, this refers to the object that is being created
+
+* 4. this - explicit binding
+    Here the value of this is set to a particular value when the function is called or applied.
 *
 * write out a code example of each explanation above
 */
